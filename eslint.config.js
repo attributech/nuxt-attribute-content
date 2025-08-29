80091210518280824cb299984c3ac7459c036f9d
Lukas von Blarer <luk@attribute.ch>
2025-08-29 13:24:35 +0200

    Fixing eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,19 @@
-import withNuxt from './.playground/.nuxt/eslint.config.mjs'
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
-export default withNuxt()
+// Run `npx @eslint/config-inspector` to inspect the resolved config interactively
+export default createConfigForNuxt({
+  features: {
+    // Rules for module authors
+    tooling: true,
+    // Rules for formatting
+    stylistic: true,
+  },
+  dirs: {
+    src: ['./playground'],
+  },
+}).append({
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-empty-object-type': 'off',
+  },
+})
