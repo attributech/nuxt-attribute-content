5d7d9ed5b3baae34b6f1c671575ac64de60fa20c
Lukas von Blarer <luk@attribute.ch>
2025-08-31 00:48:24 +0200

    fix: Adding @nuxt/image as a dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,23 @@ import { removeSizes, forceCurrentColor } from 'nuxt-svg-icon-sprite/processors'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-svg-icon-sprite', '@nuxt/content'],
+  modules: ['nuxt-svg-icon-sprite', '@nuxt/content', '@nuxt/image'],
   devtools: { enabled: true },
+  image: {
+    screens: {
+      xs: 320,
+      sm: 480,
+      md: 640,
+      lg: 960,
+      xl: 1280,
+      xxl: 1536,
+      xxxl: 1920,
+      xxxxl: 2560,
+      xxxxxl: 3840,
+    },
+    densities: [1],
+    inject: true,
+  },
   svgIconSprite: {
     sprites: {
       default: {
