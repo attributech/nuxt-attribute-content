0dff7585ea7af428cb6694115b59c3abaf3c37c2
Lukas von Blarer <luk@attribute.ch>
2025-08-30 10:59:12 +0200

    fix (icons): Adding config for nuxt-svg-icon-sprite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
+import { removeSizes, forceCurrentColor } from 'nuxt-svg-icon-sprite/processors'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['nuxt-svg-icon-sprite', '@nuxt/content'],
   devtools: { enabled: true },
+  svgIconSprite: {
+    sprites: {
+      default: {
+        importPatterns: ['./assets/icons/*.svg'],
+        processSpriteSymbol: [removeSizes(), forceCurrentColor()],
+      },
+    },
+  },
 })
