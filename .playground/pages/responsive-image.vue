c67564ac527147f61d6a233d0869bf82ff7c8ebe
Lukas von Blarer <luk@attribute.ch>
2025-11-05 03:00:38 +0100

    feat(images): providing multiple image formats using sources

--- .playground/pages/responsive-image.vue
+++ .playground/pages/responsive-image.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <h1>&lt;AttributeResponsiveImage&gt;</h1>
+    <h2>auto-sizes</h2>
     <AttributeResponsiveImage
       src="/files/test.png"
       alt="Placeholder"
       :width="1920"
       :height="1080"
+      thumbhash="PQgCBID3KaV4h3iPYYcAAAAAAA=="
+    />
+    <h2>sizes="200vw"</h2>
+    <AttributeResponsiveImage
+      src="/files/test.png"
+      alt="Placeholder"
+      :width="1920"
+      :height="1080"
+      sizes="200vw"
       thumbhash="PQgCBID3KaV4h3iPYYcAAAAAAA=="
     />
   </div>
--- components/AttributeResponsiveImage.vue
+++ components/AttributeResponsiveImage.vue
@@ -1,6 +1,7 @@
 <template>
   <UnLazyImage
     :src
+    :sources
     :src-set
     :alt
     :width
@@ -14,6 +15,8 @@
 </template>
 
 <script setup lang="ts">
+import type { ImageFormat } from '../types'
+
 interface Props {
   src: string
   alt: string
@@ -28,19 +31,53 @@ const props = withDefaults(defineProps<Props>(), {
   loading: 'lazy',
 })
 
-const srcSet = computed<string>(() => {
+const appConfig = useAppConfig()
+
+const srcSet = computed<string | undefined>(() => {
+  if (props.sizes) {
+    const format = appConfig.attributeContent.images.formats.at(-1)
+    if (format !== undefined) {
+      return getSrcSet(format)
+    }
+  }
+  return undefined
+})
+
+const getSrcSet = (imageFormat: ImageFormat): string => {
   const img = useImage()
   return img.getSizes(props.src, {
     sizes:
       'xs:100vw sm:100vw md:100vw lg:100vw xl:100vw xxl:100vw xxxl:100vw xxxxl:100vw xxxxxl:100vw',
     modifiers: {
-      format: 'webp',
-      quality: 60,
+      format: imageFormat.format,
+      quality: imageFormat.quality,
     },
   }).srcset
+}
+
+interface ImageSource {
+  type: string
+  srcSet: string
+  sizes?: string
+}
+
+const sources = computed<ImageSource[] | undefined>(() => {
+  if (props.sizes) {
+    return undefined
+  }
+  return appConfig.attributeContent.images.formats.map((imageFormat) => {
+    const source: ImageSource = {
+      type: `image/${imageFormat.format}`,
+      srcSet: getSrcSet(imageFormat),
+    }
+    if (props.sizes) {
+      source.sizes = props.sizes
+    }
+    return source
+  })
 })
 
-const style = computed<string | false>(() => {
+const style = computed<false | string>(() => {
   if (props.width !== undefined && props.height !== undefined) {
     return `aspect-ratio: ${props.width}/${props.height};`
   }
